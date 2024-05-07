f6625e84c5b26a0b86de046e762923888399a5c6
Badal Sahoo <od.badal07@gmail.com>
2024-05-07 21:46:04 +0530

    Adding Previous & Next Button

--- src/components/News.js
+++ src/components/News.js
@@ -6,15 +6,41 @@ export class News extends Component {
     super();
     this.state = {
       articles : [],
-      loading: false
+      loading: false,
+      page:1
     }
   }
   async componentDidMount(){
     let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=3a428199d676415e8632679edb30f67f";
     let data = await fetch(url);
     let parsedData = await data.json()
-    console.log(data);
-    this.setState({articles: parsedData.articles})
+    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+  }
+  handelPrev = async () =>{
+    console.log("Previous button click");
+    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=3a428199d676415e8632679edb30f67f&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+    })
+  }
+  handelNext = async () =>{
+    console.log("next button click");
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+    }
+    else{
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=3a428199d676415e8632679edb30f67f&page=${this.state.page + 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        this.setState({
+          page: this.state.page + 1,
+          articles: parsedData.articles
+     })
+    }
+    
   }
   render() {
     return (
@@ -28,6 +54,10 @@ export class News extends Component {
                   </div>
         })}
           
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page<=1} type="button" className="btn btn-secondary" onClick = {this.handelPrev}>&#8678; Previous</button>
+          <button type="button" className="btn btn-secondary" onClick = {this.handelNext}>Next &#8680;</button>
         </div>
       </div>
     )
